03a138cb5db9cc85f73a9b2c7a5eb8e2e4219596
tanbir ahammad <tavirahammadjoy@gmail.com>
2026-06-10 17:08:26 +0800

    chore: remove unused blog components and global error handling; update globals.css formatting

--- js_projects/7-project-me-full-stack-app/my-app/src/app/blog/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/blog/slug/page.tsx
-import { PageProps } from "next/app";
-
-
-export default function BlogPage(props: PageProps<"/blog/[slug]">) {
-  const { slug } = props.params;
-  return <div>This is the blog post: {slug}</div>;
-}
--- js_projects/7-project-me-full-stack-app/my-app/src/app/blog/page.tsx
+++
@@ -1,4 +0,0 @@
-// app/blog/page.tsx
-export default function BlogPage() {
-  return <div>This is the blog page</div>;
-}
